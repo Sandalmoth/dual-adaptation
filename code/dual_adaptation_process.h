f314709b978f340b30c8f73e350bac2dc4d7976b
Jonathan Lindström <jonathan.lindstrom@lnu.se>
2019-06-14 15:21:55 +0200

    start work on mpi exact simulation of small numbers of cells

---
+++ code/dual_adaptation_process.h
@@ -0,0 +1,16 @@
+#ifndef __DUAL_ADAPTATION_PROCESS_H__
+#define __DUAL_ADAPTATION_PROCESS_H__
+
+
+#include <valarray>
+#include <cstddef>
+#include <random>
+
+
+class DAP {
+public:
+private:
+};
+
+
+#endif
